c43ca34e4fbd3858f9c7997b86fa1224edbeed33
sogrey <suzhaoheng2006@163.com>
2021-02-04 01:56:32 +0800

    add @babel/polyfill

--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
+import '@babel/polyfill'
+
 import hello from "./my-module"
 
 console.log(hello);
+
+const promiseArray = [
+    new Promise(() => {}),
+    new Promise(() => {})
+]
+
+promiseArray.map(promise => {
+    console.log(promise);
+})
