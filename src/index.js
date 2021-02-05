bd6b6579de1c63ea29a32b8ca4a882c32a30180d
sogrey <suzhaoheng2006@163.com>
2021-02-05 16:46:31 +0800

    add  webpack-dev-server

--- src/index.js
+++ src/index.js
@@ -16,30 +16,30 @@ promiseArray.map(promise => {
 
 
 
-// async function loadFile(url) {
-//     const req = await fetch(url);
-//     return req.text();
-// }
+async function loadFile(url) {
+    const req = await fetch(url);
+    return req.text();
+}
 
-// function parseData(text) {
-//     console.log(JSON.parse(text));
-// }
+function parseData(text) {
+    console.log(JSON.parse(text));
+}
 
-// async function loadData(info) {
-//     const text = await loadFile(info.url);
-//     info.file = parseData(text);
-// }
+async function loadData(info) {
+    const text = await loadFile(info.url);
+    info.file = parseData(text);
+}
 
-// async function loadAll() {
-//     const fileInfos = [{
-//             url: 'https://api.github.com/'
-//         },
-//         {
-//             url: 'https://api.github.com/users/sogrey'
-//         },
-//     ];
+async function loadAll() {
+    const fileInfos = [{
+            url: 'https://api.github.com/'
+        },
+        {
+            url: 'https://api.github.com/users/sogrey'
+        },
+    ];
 
-//     await Promise.all(fileInfos.map(loadData));
+    await Promise.all(fileInfos.map(loadData));
 
-// }
-// loadAll()
+}
+loadAll()
--- webpack.config.js
+++ webpack.config.js
@@ -9,7 +9,7 @@ module.exports = {
     module: {
         rules: [{
             test: /\.js$/,
-            exclude: /node_modules/,
+            exclude: /(node_modules|bower_components)/,
             use: {
                 loader: "babel-loader",
                 options: {
